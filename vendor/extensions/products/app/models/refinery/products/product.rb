0613fad6a1c17ab361f40b5126d7ef74cdfcb981
Artjom Juferov <derevjaginti@gmail.com>
2013-12-11 19:22:18 +0300

    don't forget to edit last next

--- vendor/extensions/products/app/models/refinery/products/product.rb
+++ vendor/extensions/products/app/models/refinery/products/product.rb
@@ -5,13 +5,30 @@ module Refinery
 
       attr_accessible :name, :description, :description_short, :position
 
+      validates :name, :presence => true, :uniqueness => true
+
       translates :name, :description, :description_short
 
       class Translation
         attr_accessible :locale
       end
 
-      validates :name, :presence => true, :uniqueness => true
+      def prev_product
+        self.class.first(:conditions => ["name < ?", name], :order => "name desc")
+      end
+
+      def next_product
+        self.class.first(:conditions => ["name > ?", name], :order => "name asc")
+      end
+
+      def previous
+        Post.where(["id < ?", id]).last
+      end
+
+      def next
+        Post.where(["id > ?", id]).first
+      end
+      
     end
   end
 end
