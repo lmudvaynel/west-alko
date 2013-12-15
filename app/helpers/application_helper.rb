606e12db1a6c960ff89a6ee7801a7dcfa7d57913
Artjom Juferov <derevjaginti@gmail.com>
2013-12-16 01:43:56 +0300

    edit home view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,14 @@
-module ApplicationHelper\
-   def header_menu
-    menu = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
-    menu.menu_tag = :div
-    menu.css = "row clearfix"
-    menu.list_tag = "ul class='sf-menu'"
-    menu
+module ApplicationHelper
+
+  def which_type_list(locale)
+    @types_en = {own: 'Own', foreign: 'Foreign', flood:  'Flood'} 
+    @types_ru = {own: 'Sv', foreign: 'Ch', flood: 'Raz'}
+    locale == :en ? @types_en : @types_ru
   end
+
+  def link_for_lang(locale)
+    locale.to_s == 'en' ? lang = :ru : lang = :en
+    link_to Refinery::I18n.locales[lang], refinery.url_for(:locale => lang)
+  end
+
 end
