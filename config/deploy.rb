38530c0c1a9bee7b5fdec5fecb4bab4b9cbfca60
Artjom Juferov <derevjaginti@gmail.com>
2014-06-19 20:06:23 +0300

    dear

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require 'capistrano/ext/multistage'
 set :application, "WestAlko" #write app name there
 
 set :scm, :git
-set :repository,  "git@github.com:simutin/west-alko.git" #place your repo there
+set :repository,  "git@github.com:derevjaginti/west-alko.git" #place your repo there
 
 set :deploy_via, :remote_cache
 
