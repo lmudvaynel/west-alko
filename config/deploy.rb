662f1baa9bce8193fb3bb0012d6b136dc9ba4678
Demko <artdemko@gmail.com>
2014-07-15 17:42:51 +0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ require 'capistrano/ext/multistage'
 set :application, "WA" #write app name there
 
 set :scm, :git
-set :repository,  "git@github.com:derevjaginti/west-alko.git" #place your repo there
+set :repository,  "git@github.com:lmudvaynel/west-alko.git" #place your repo there
 
 set :deploy_via, :remote_cache
 
